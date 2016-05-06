93bad331a35294b9cc05155485081939cdd4d49d
ravikiran <ravikiran.sn042@gmail.com>
2016-05-06 23:42:48 +0530

    // correcting test cases and format.

--- integration/spark/src/main/java/org/carbondata/integration/spark/merger/NodeBlockRelation.java
+++ integration/spark/src/main/java/org/carbondata/integration/spark/merger/NodeBlockRelation.java
@@ -7,26 +7,24 @@ import org.carbondata.core.carbon.datastore.block.TableBlockInfo;
  */
 public class NodeBlockRelation implements Comparable<NodeBlockRelation> {
 
+  private final TableBlockInfo block;
+  private final String node;
 
-    private final TableBlockInfo block;
-    private final String node;
+  public NodeBlockRelation(TableBlockInfo block, String node) {
+    this.block = block;
+    this.node = node;
 
-    public NodeBlockRelation(TableBlockInfo block, String node) {
-        this.block = block;
-        this.node = node;
+  }
 
-    }
+  public TableBlockInfo getBlock() {
+    return block;
+  }
 
-    public TableBlockInfo getBlock() {
-        return block;
-    }
+  public String getNode() {
+    return node;
+  }
 
-    public String getNode() {
-        return node;
-    }
-
-    @Override
-    public int compareTo(NodeBlockRelation obj) {
-        return this.getNode().compareTo(obj.getNode());
-    }
+  @Override public int compareTo(NodeBlockRelation obj) {
+    return this.getNode().compareTo(obj.getNode());
+  }
 }
